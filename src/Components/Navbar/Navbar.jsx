8ee3cf17730ba40eb46d5508d956fc56061c648e
vikas9872 <shanabhogvikas648.com>
2025-02-22 20:27:34 +0530

    initial commit

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -64,7 +64,7 @@ export default function Navbar() {
                             as="button"
                             onClick={() => navigate(item.path)}
                             className={classNames(
-                                item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                                item.current ? 'bg-gray-900 black' : 'text-black hover:bg-gray-700 hover:text-white',
                                 'block rounded-md px-3 py-2 text-base font-medium cursor-pointer'
                             )}
                         >
