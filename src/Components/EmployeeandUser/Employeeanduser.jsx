fdf7218222fb78ec384a23457ef5c65da3ab7ce9
vikas9872 <shanabhogvikas648.com>
2025-03-22 20:59:50 +0530

    commit

--- src/Components/EmployeeandUser/Employeeanduser.jsx
+++ src/Components/EmployeeandUser/Employeeanduser.jsx
@@ -12,11 +12,11 @@ const Employeeanduser = () => {
     <div className="flex items-center h-full bg-[url(/Images/empanduserBkgd.jpg)] bg-cover">
       <div className="flex flex-col md:flex-row w-full h-full justify-evenly items-center">
         <button className="flex flex-col items-center justify-center cursor-pointer gap-2 h-[60%] hover:shadow-lg" data-aos="zoom-in" onClick={() => navigate("/manager")}>
-          <img src="./Images/employee.png" className="h-[150px] md:h-[300px] w-[150px] md:w-[300px]" alt="user_icon" />
+          <img src="./Images/profile.png" className="h-[150px] md:h-[300px] w-[150px] md:w-[300px]" alt="user_icon" />
           <h1 className="text-black font-bold backdrop-blur-0 backdrop-saturate-200 bg-white/60 rounded-lg border border-gray-300/30 shadow-lg p-4">Employee</h1>
         </button>
         <button className="flex flex-col items-center justify-center cursor-pointer gap-2 h-[60%] hover:shadow-lg" data-aos="zoom-in" onClick={() => navigate("/user")}>
-          <img src="./Images/user.png" className="h-[150px] md:h-[300px] w-[150px] md:w-[300px]" alt="user_icon" />
+          <img src="./Images/profile.png" className="h-[150px] md:h-[300px] w-[150px] md:w-[300px]" alt="user_icon" />
           <h1 className="text-black font-bold backdrop-blur-0 backdrop-saturate-200 bg-white/60 rounded-lg border border-gray-300/30 shadow-lg p-4">User</h1>
         </button>
       </div>
